06d9895bf18987c6217ec5ac23bc8ed4c0b0a243
Dan T <dp.tyler@hotmail.co.uk>
2021-01-27 14:18:31 +0000

    Start of pairing with Morne

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -98,6 +98,7 @@ RSpec.configure do |config|
   Kernel.srand config.seed
 =end
 ENV['RACK_ENV'] = 'test'
+
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
 require 'capybara/rspec'
 require 'sinatra'
